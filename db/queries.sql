148a2423c434f99495093d33b28f5abee46400a9
Bread <jmagri1@uncc.edu>
2024-10-23 16:50:07 -0400

    Database schema creates tables and employee table has functional foregin key relationship.

--- db/queries.sql
+++ db/queries.sql
@@ -1,5 +1,6 @@
 -- WHEN I start the application
--- THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+-- THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, 
+-- add a role, add an employee, and update an employee role
 
 
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,49 @@
 -- As the image illustrates, your schema should contain the following three tables:
 
--- * `department`
+-- Deletes and creates a fresh database
+-- Functionally tested
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
 
---   * `id`: `SERIAL PRIMARY KEY`
+\c employee_tracker_db;
 
---   * `name`: `VARCHAR(30) UNIQUE NOT NULL` to hold department name
+-- Creates department table with 2 attributes.
+-- Drops table if exists, starting fresh.
+-- Functionally tested.
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
 
--- * `role`
+-- Creates role table with 4 attributes.
+-- Drops table if exists, starting fresh.
+-- Functionally tested.
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
+);
 
---   * `id`: `SERIAL PRIMARY KEY`
-
---   * `title`: `VARCHAR(30) UNIQUE NOT NULL` to hold role title
-
---   * `salary`: `DECIMAL NOT NULL` to hold role salary
-
---   * `department_id`: `INTEGER NOT NULL` to hold reference to department role belongs to
+-- Creates role table with 6 attributes.
+-- Drops table if exists, starting fresh.
+-- Functionally tested.
+-- Added FOREIGN KEY relation to manager id.
+-- Functionally tested.
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
 
 -- * `employee`
 
